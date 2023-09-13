f21bf07afc92e68c47e1c11c6404c51034a82fc8
EldarDanilovich <ildar.nasrov@gmail.com>
2023-09-13 10:48:37 +0500

    Edit modules package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,29 @@
-import { defineConfig } from "vite";
-import path from "path";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
-      entry: path.resolve("__dirname", "src/lib/index.ts"),
-      name: "tw-rus-ui",
+      entry: path.resolve(__dirname, 'src/lib/index.ts'),
+      name: 'tw-rus-ui',
       fileName: (format) => `tw-rus-ui.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: "React",
+          react: 'React',
         },
       },
     },
   },
-  plugins: [react()],
-});
+})
