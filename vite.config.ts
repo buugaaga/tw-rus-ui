896bbf5795913317131aef795cb7595ae0568834
EldarDanilovich <ildar.nasrov@gmail.com>
2023-09-13 11:01:49 +0500

    fix type declaration

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,7 @@ import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
+  plugins: [react(), dts({ rollupTypes: true, insertTypesEntry: true })],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/lib/index.ts'),
